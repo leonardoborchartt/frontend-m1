0b0bb828c6ada0b7eb046c4caefd03bedc7ea18b
Leonardo Borchartt <leob_b@hotmail.com>
2019-11-01 18:36:49 +0000

    alteracao titulo

--- src/routes.js
+++ src/routes.js
@@ -14,25 +14,25 @@ import VendaForm from './pages/VendaForm';
 
 export default function Routes() {
     return (
-        
+
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={PageHome} /> 
-    
+                <Route path="/" exact component={PageHome} />
+
                 <Route path="/home" exact component={PageHome} />
                 <Route path="/produto" exact component={ProductList} />
                 <Route path="/produto/create"  component={ProductForm} />
                 <Route path="/produto/edit/:id"  component={ProductForm} />
-                
+
                 <Route path="/users" exact component={UserList} />
                 <Route path="/users/create" component={UserForm} />
                 <Route path="/users/edit/:id" component={UserForm} />
-                
+
                 <Route path="/cidade" exact component={CidadeList} />
                 <Route path="/cidade/create" component={CidadeForm} />
                 <Route path="/cidade/edit/:id" component={CidadeForm} />
 
-                
+
                 <Route path="/venda" exact component={VendaForm} />
             </Switch>
         </BrowserRouter>
